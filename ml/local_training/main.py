513fa90387e0b864fbbd2a48d2319d9bb1fe8ca4
tkow <t.kusama0613@outlook.jp>
2019-06-09 05:43:17 +0900

    modelちゃんとした

--- ml/local_training/main.py
+++ ml/local_training/main.py
@@ -4,14 +4,27 @@ from sklearn.naive_bayes import GaussianNB  # ガウシアン
 import pickle
 from numpy.random import normal, randint
 
-bodyTemperature = normal(35.5, 3, size=1000)   # 平均35、標準偏差3の正規分布
-sleepAnalysy = normal(5, 2, size=1000)   # 平均35、標準偏差3の正規分布
+bodyTemperature = normal(35.5, 1.5, size=1000)   # 平均35、標準偏差3の正規分布
+bodyTemperature[bodyTemperature < 34] = 34
+bodyTemperature[bodyTemperature > 40] = 40
+sleepAnalysis = normal(5, 2, size=1000)   # 平均35、標準偏差3の正規分布
+sleepAnalysis[sleepAnalysis > 18] = 18   # 平均35、標準偏差3の正規分布
+sleepAnalysis[sleepAnalysis < 0] = 0   # 平均35、標準偏差3の正規分布
 heartRate = normal(60, 20, size=1000)   # 平均35、標準偏差3の正規分布
-binary = randint(2, size=1000)   # 平均35、標準偏差3の正規分布
+heartRate[heartRate > 180] = 180   # 平均35、標準偏差3の正規分布
+heartRate[heartRate < 40] = 40   # 平均35、標準偏差3の正規分布
+binary = np.repeat(0, 1000)
+binary[
+    ((35 < bodyTemperature) & (bodyTemperature < 38)) &
+    (
+        ((5 < sleepAnalysis) & (sleepAnalysis < 10)) |
+        ((50 < heartRate) & (sleepAnalysis < 120))
+    )
+] = 1
 
 X = np.array([
     bodyTemperature,
-    sleepAnalysy,
+    sleepAnalysis,
     heartRate
 ]).T  # 特徴ベクトル
 
