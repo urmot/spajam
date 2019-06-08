1d005f51e25badfaf43cab35151c275fcab0b5bc
tkow <t.kusama0613@outlook.jp>
2019-06-09 05:12:54 +0900

    study

--- ml/local_training/main.py
+++ ml/local_training/main.py
@@ -1,15 +1,24 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from sklearn.naive_bayes import GaussianNB # ガウシアン
+from sklearn.naive_bayes import GaussianNB  # ガウシアン
 import pickle
+from numpy.random import normal, randint
 
-X = np.array([[36.8,5,71],
-              [38.6,2,90],
-              [39.0,9,80]]) # 特徴ベクトル
-y = np.array([1, 0, 0]) # そのラベル
+bodyTemperature = normal(35.5, 3, size=1000)   # 平均35、標準偏差3の正規分布
+sleepAnalysy = normal(5, 2, size=1000)   # 平均35、標準偏差3の正規分布
+heartRate = normal(60, 20, size=1000)   # 平均35、標準偏差3の正規分布
+binary = randint(2, size=1000)   # 平均35、標準偏差3の正規分布
 
-clf = GaussianNB() # 正規分布を仮定したベイズ分類
-clf.fit(X, y) # 学習をする
+X = np.array([
+    bodyTemperature,
+    sleepAnalysy,
+    heartRate
+]).T  # 特徴ベクトル
+
+y = np.array(binary)  # そのラベル
+
+clf = GaussianNB()  # 正規分布を仮定したベイズ分類
+clf.fit(X, y)  # 学習をする
 filename = '../health_prediction/predict_model/online_naive_bayes_model.sav'
 
 pickle.dump(clf, open(filename, 'wb'))
