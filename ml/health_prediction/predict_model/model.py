292579e905e841ea5fc9218b67d7776486644674
tkow <t.kusama0613@outlook.jp>
2019-06-09 01:37:30 +0900

    add prediction model

--- ml/health_prediction/predict_model/model.py
+++ ml/health_prediction/predict_model/model.py
@@ -1,2 +1,13 @@
+import pickle
+import numpy as np
+
+clf = pickle.load(open('./online_naive_bayes_model.sav', 'rb'))
+
 def predict(request):
+    # request
+    t = np.array([[38.0,0,80]]) # テストデータ
+    result = clf.predict(t) # => [3]
+    print(result)
     return "%"
+
+predict('')
