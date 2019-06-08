e649f7770349fb1a7d2af7dbc1c2967e9be7dc25
t-kusakabe <ttmac0704@gmail.com>
2019-06-09 01:00:13 +0900

    map表示する

--- lib/ui/page/mission_page.dart
+++ lib/ui/page/mission_page.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MissionPage extends StatefulWidget {
   final String title;
@@ -9,39 +13,77 @@ class MissionPage extends StatefulWidget {
 }
 
 class _MissionPageState extends State<MissionPage> {
-  int _counter = 0;
+  LocationData currentLocation;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  // StreamSubscription<LocationData> locationSubscription;
+
+  Location _locationService = new Location();
+  String error;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initPlatformState();
+    _locationService.onLocationChanged().listen((LocationData result) async {
+      setState(() {
+        currentLocation = result;
+      });
     });
+  }
+
+  Completer<GoogleMapController> _controller = Completer();
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'This is Mission page',
+    if (currentLocation == null) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Maps Sample App'),
+            backgroundColor: Colors.green[700],
+          ),
+          body: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(35.6580339, 139.7016358),
+                zoom: 17.0,
+              ),
+              myLocationEnabled: true,
+              mapType: MapType.hybrid,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      );
+    }
+  }
+
+  void initPlatformState() async {
+    LocationData myLocation;
+    try {
+      myLocation = await _locationService.getLocation();
+      error = "";
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENITED')
+        error = 'Permission denited';
+      else if (e.code == 'PERMISSION_DENITED_NEVER_ASK')
+        error =
+            'Permission denited - please ask the user to enable it from the app settings';
+      myLocation = null;
+    }
+    setState(() {
+      currentLocation = myLocation;
+    });
   }
 }
